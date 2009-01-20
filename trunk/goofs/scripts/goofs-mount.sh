5fb09e7b14eb5e2e3e27f6496be963feb6f823e6
bigwynnr <bigwynnr@bb54a439-f648-0410-ab55-cb5ea932f9a3>
2009-01-20 06:32:56 +0000

    updated the launch scripts.  created a manifest.

---
+++ trunk/goofs/scripts/goofs-mount.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#####################################################################
+# Adjust this to point to the home directory of a java 1.5 jre
+JAVA_HOME=/usr/lib/jvm/java-1.5.0-sun-1.5.0_update17/jre
+
+# Adjust this to point to the directory in which you extracted goofs
+GOOFS_HOME=/home/rwynn/Desktop/goofs
+#####################################################################
+
+$JAVA_HOME/bin/java -Dusername=$1 -Dpassword=$2 -Djava.library.path=$GOOFS_HOME/jni/fuse-2.7.2 -jar $GOOFS_HOME/goofs.jar -f $3
