80ced8f6cc4a16ee5f33add587e2e4742f0cda9c
bigwynnr <bigwynnr@bb54a439-f648-0410-ab55-cb5ea932f9a3>
2009-10-04 20:30:19 +0000

    goofs-0.7.3

---
+++ tags/goofs-0.7.3/src/main/java/goofs/fs/calendar/CalendarEventDurationDir.java
@@ -0,0 +1,143 @@
+package goofs.fs.calendar;
+
+import fuse.Errno;
+import goofs.EntryContainer;
+import goofs.calendar.ICalendar;
+import goofs.fs.Dir;
+import goofs.fs.Node;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import com.google.gdata.data.calendar.CalendarEntry;
+import com.google.gdata.data.calendar.CalendarEventEntry;
+
+public class CalendarEventDurationDir extends Dir implements EntryContainer {
+
+	protected Date start;
+
+	protected Date end;
+
+	protected Set<String> entryIds = new HashSet<String>();
+
+	public CalendarEventDurationDir(Dir parent, String name, CalendarEntry cal,
+			Date start, Date end) throws Exception {
+
+		super(parent, name, 0777);
+
+		setStart(start);
+
+		setEnd(end);
+
+		List<CalendarEventEntry> events = getCalendarService().getEvents(cal,
+				start, end);
+
+		for (CalendarEventEntry event : events) {
+
+			CalendarEventDir eventDir = new CalendarEventDir(this, event);
+
+			add(eventDir);
+
+			getEntryIds().add(event.getSelfLink().getHref());
+
+		}
+
+	}
+
+	public Set<String> getEntryIds() {
+		return entryIds;
+	}
+
+	public void addNewEntryById(String entryId) throws Exception {
+
+		CalendarEventEntry event = getCalendarService().getCalendarEventById(
+				entryId);
+
+		CalendarEventDir eventDir = new CalendarEventDir(this, event);
+
+		add(eventDir);
+
+		getEntryIds().add(event.getSelfLink().getHref());
+
+	}
+
+	public Set<String> getCurrentEntryIds() throws Exception {
+		Set<String> ids = new HashSet<String>();
+
+		List<CalendarEventEntry> events = getCalendarService().getEvents(
+				getCalendar(), start, end);
+
+		for (CalendarEventEntry event : events) {
+			ids.add(event.getSelfLink().getHref());
+		}
+
+		return ids;
+	}
+
+	protected ICalendar getCalendarService() {
+
+		return ((CalendarsDir) getParent().getParent()).getCalendarService();
+	}
+
+	protected CalendarEntry getCalendar() throws Exception {
+
+		return ((CalendarDir) getParent()).getCalendar();
+
+	}
+
+	protected Date getStart() {
+		return start;
+	}
+
+	protected void setStart(Date start) {
+		this.start = start;
+	}
+
+	protected Date getEnd() {
+		return end;
+	}
+
+	protected void setEnd(Date end) {
+		this.end = end;
+	}
+
+	@Override
+	public int createChild(String name, boolean isDir) {
+		return Errno.EROFS;
+	}
+
+	@Override
+	public int createChildFromExisting(String name, Node child) {
+		return Errno.EROFS;
+	}
+
+	@Override
+	public int createTempChild(String name) {
+		return Errno.EROFS;
+	}
+
+	@Override
+	public int delete() {
+
+		remove();
+
+		return 0;
+	}
+
+	@Override
+	public int rename(Dir newParent, String name) {
+
+		if (getParent() == newParent) {
+
+			((CalendarDir) parent).createChild(name, true);
+
+			remove();
+
+		}
+
+		return 0;
+	}
+
+}
---
+++ tags/goofs-0.7.3/src/main/java/goofs/fs/calendar/CalendarEventRecurrenceFile.java
@@ -0,0 +1,65 @@
+package goofs.fs.calendar;
+
+import fuse.Errno;
+import goofs.GoofsProperties;
+import goofs.calendar.ICalendar;
+import goofs.fs.Dir;
+import goofs.fs.File;
+
+import java.io.StringWriter;
+
+import com.google.gdata.data.calendar.CalendarEventEntry;
+import com.google.gdata.util.common.xml.XmlWriter;
+
+public class CalendarEventRecurrenceFile extends File {
+
+	public CalendarEventRecurrenceFile(Dir parent, CalendarEventEntry event)
+			throws Exception {
+
+		super(parent, GoofsProperties.INSTANCE
+				.getProperty("goofs.calendar.recurrence"), 0777, "");
+
+		setContent(getRecurrence(event).getBytes());
+
+	}
+
+	protected ICalendar getCalendarService() {
+
+		return ((CalendarEventDir) getParent()).getCalendarService();
+	}
+
+	protected String getRecurrence(CalendarEventEntry event) throws Exception {
+
+		StringWriter sw = new StringWriter();
+		XmlWriter writer = new XmlWriter(sw);
+
+		if (event.getRecurrence() != null) {
+
+			event.getRecurrence().generate(writer,
+					getCalendarService().getExtensionProfile());
+
+		}
+		String when = sw.getBuffer().toString();
+
+		writer.close();
+
+		return when;
+
+	}
+
+	@Override
+	public int save() {
+		return Errno.EROFS;
+	}
+
+	@Override
+	public int delete() {
+		return Errno.EROFS;
+	}
+
+	@Override
+	public int rename(Dir newParent, String name) {
+		return Errno.EROFS;
+	}
+
+}
