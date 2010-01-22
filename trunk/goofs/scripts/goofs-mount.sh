86711d7286f8149b0b9c32fc22437d860dabcaca
bigwynnr <bigwynnr@bb54a439-f648-0410-ab55-cb5ea932f9a3>
2010-01-22 03:02:37 +0000

    add support for 64-bit linux

--- trunk/goofs/scripts/goofs-mount.sh
+++ trunk/goofs/scripts/goofs-mount.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
 
 #####################################################################
-# Uncomment the following line to use a specific JRE
-#JAVA_HOME=/usr/lib/jvm/java-1.5.0-sun/jre
+# Edit the following line to use a specific JRE
+JAVA=java
 
 # Adjust this to point to the directory into which you extracted goofs.jar
 GOOFS_HOME=.
 
-# Adjust this to point to the directory containing the version of libjavafs.so corresponding to your version of fuse
-JNI_LIB=$GOOFS_HOME/jni/lib32/fuse-2.7.4
+# Adjust this to your installed fuse version
+FUSE_VERSION=2.7.4
 #####################################################################
 
-$JAVA_HOME/bin/java -Djava.library.path=$JNI_LIB -jar $GOOFS_HOME/goofs.jar -f $1
+arch=$(uname -m)
+if [ "$arch"=="x86_64" ]
+then
+    JNI_HOME=$GOOFS_HOME/jni/lib64
+else
+    JNI_HOME=$GOOFS_HOME/jni/lib32
+fi
+
+JNI_LIB=$JNI_HOME/fuse-$FUSE_VERSION
+
+$JAVA -Djava.library.path=$JNI_LIB -jar $GOOFS_HOME/goofs.jar -f $1
